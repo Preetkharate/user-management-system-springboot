6ef8a018bd1baef08003cc35ab514dba8c975c93
Preetkharate <kharatepreet4291@gmail.com>
2026-04-01 21:31:29 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      {users.map(user => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
